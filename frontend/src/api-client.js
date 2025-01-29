80e845d352ca5039eb87bad98f276a21c0c8d51a
Roshan-T <roshantiwari@gmial.com>
2025-01-30 01:33:29 +0545

    backend link updated

--- frontend/src/api-client.js
+++ frontend/src/api-client.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/`;
 
 export const registerUser = async (userformData) => {
   const response = await fetch(`${API_BASE_URL}users/signup`, {
